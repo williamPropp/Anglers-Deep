5ea92331b20fc821a93df1fc480f747ea3242aad
William Propp <33852102+williamPropp@users.noreply.github.com>
2022-05-18 14:39:04 -0700

    uploaded build_015

--- scripts/credits.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("ui_cancel"):
-		Global.change_scene(Global.main_menu_path)
-
-
-func _on_TextureButton_pressed():
-	Global.change_scene(Global.main_menu_path)
